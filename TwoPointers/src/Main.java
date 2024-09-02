0ff4afb89ac270b59da9ee709d1e478d58761667
Rachana Thota <rachanathota@Rachanas-MacBook-Air.local>
2024-09-02 10:50:20 -0500

    Added two pointer problems

--- TwoPointers/src/Main.java
+++ TwoPointers/src/Main.java
@@ -53,9 +53,12 @@ public class Main {
         System.out.println(res12);
         int res13 = maxArea(new int[]{1,7,2,5,4,7,3,6});
         System.out.println(res13);
-
-
-
+        int res14 = trap(new int[]{0,2,0,3,1,0,1,3,2,1});
+        System.out.println(res14);
+        int res15 = maxScore(Arrays.asList(2, 4, 5, 8, 10), Arrays.asList(4, 6, 8, 9));
+        System.out.println(res15);
+        int res16 = minSwaps(new int[]{0,1,0,1,1,0,0});
+        System.out.println(res16);
     }
 
     public static class Node<T>{
@@ -418,7 +421,152 @@ public class Main {
 
 
 
+    //Trapping Rain Water
+    //Solved
+    //You are given an array non-negative integers heights which represent an elevation map. Each value heights[i] represents the height of a bar, which has a width of 1.
+    //
+    //Return the maximum area of water that can be trapped between the bars.
+    //
+    //Example 1:
+    //
+    //
+    //
+    //Input: height = [0,2,0,3,1,0,1,3,2,1]
+    //
+    //Output: 9
 
+    public static int trap(int[] height) {
+        int l=0;
+        int r=height.length-1;
+        int leftMax=height[0];
+        int rightMax = height[height.length-1];
+        int res=0;
+        while(l<r){
+            if(height[l]<height[r]){
+                l++;
+                leftMax = Math.max(leftMax, height[l]);
+                res = res + leftMax - height[l];
+            } else{
+                r--;
+                rightMax = Math.max(rightMax, height[r]);
+                res = res + rightMax - height[r];
+            }
+        }
+        return res;
+    }
+
+    //You are given two sorted arrays of distinct integers, arr1, and arr2. Your goal is to start from the beginning of one array, and arrive at the end of one array (it could be the same array or not).
+    //
+    //For each step, you can either move forward a step on an array, or move to a square in the other array where the number is the same as the number in your current square ("teleportation"). Your total score is defined as the sum of all unique numbers that you have been on.
+    //
+    //Find the maximum score that you can get given the above rules. Since the result might be very large and cause overflow, return the maximum score modded by 10^9 + 7.
+    //
+    //Parameters
+    //arr1: A list of ordered, distinct integers.
+    //arr2: Another list of ordered, distinct integers.
+    //Result
+    //The maximum score possible, modded by 10^9 + 7.
+    //Examples
+    //Example 1
+    //Input: arr1 = [2, 4, 5, 8, 10], arr2 = [4, 6, 8, 9]
+    //
+    //Output: 30
+    public static int maxScore(List<Integer> arr1, List<Integer> arr2) {
+        int n1 = arr1.size();
+        int n2 = arr2.size();
+        int maxScore = 0;
+        int s1 = 0;
+        int s2 = 0;
+        int i = 0;
+        int j = 0;
+
+        while(i<n1 && j<n2) {
+
+            if(arr1.get(i).equals(arr2.get(j))) {
+                maxScore = maxScore + Math.max(s1,s2) + arr1.get(i);
+                s1=0;
+                s2=0;
+                i++;
+                j++;
+            } else if(arr1.get(i) < arr2.get(j)) {
+                s1 = s1 + arr1.get(i);
+                i++;
+            } else {
+                s2 = s2 + arr2.get(j);
+                j++;
+            }
+        }
+
+        while(i<n1) {
+            s1 = s1 + arr1.get(i);
+            i++;
+        }
+
+        while(j<n2) {
+            s2 = s2 + arr2.get(j);
+            j++;
+        }
+
+        return maxScore + Math.max(s1,s2);
+    }
+
+
+    public static int minSwaps(int[] nums) {
+        //A swap is defined as taking two distinct positions in an array and swapping the values in them.
+        //
+        //A circular array is defined as an array where we consider the first element and the last element to be adjacent.
+        //
+        //Given a binary circular array nums, return the minimum number of swaps required to group all 1's present in the array together at any location.
+        //
+        //
+        //
+        //Example 1:
+        //
+        //Input: nums = [0,1,0,1,1,0,0]
+        //Output: 1
+        //Explanation: Here are a few of the ways to group all the 1's together:
+        //[0,0,1,1,1,0,0] using 1 swap.
+        //[0,1,1,1,0,0,0] using 1 swap.
+        //[1,1,0,0,0,0,1] using 2 swaps (using the circular property of the array).
+        //There is no way to group all 1's together with 0 swaps.
+        //Thus, the minimum number of swaps required is 1.
+        int numOnes=0;
+        int maxOnes =0 ;
+        int currOnes = 0;
+        for (int num : nums) {
+            if (num == 1) {
+                numOnes++;
+            }
+        }
+
+        int[] extendedArray = new int[nums.length*2];
+        System.arraycopy(nums,0,extendedArray,0,nums.length);
+        System.arraycopy(nums,0,extendedArray,nums.length,nums.length);
+
+        for(int i=0;i<extendedArray.length;i++){
+            if(extendedArray[i] == 1){
+                currOnes++;
+            }
+
+            if(i>=numOnes){
+                if(extendedArray[i-numOnes] == 1){
+                    currOnes--;
+                }
+            }
+
+            maxOnes = Math.max(maxOnes,currOnes);
+        }
+
+        return numOnes-maxOnes;
+    }
 }
+
+
+
+
+
+
+
+
 
 
